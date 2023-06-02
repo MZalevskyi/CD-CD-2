707b1f43c3ef3000f69fdb279a8dc9da6f9e7eae
Maksym Zalevskyi <90321976+MZalevskyi@users.noreply.github.com>
2023-06-02 12:59:48 +0300

    Update views.py

--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Recipe
 
-# Create your views here.
+def main_view(request):
+    recipes = Recipe.objects.filter(year=2023)
+    return render(request, 'main.html', {'recipes': recipes})
+
+def recipe_detail_view(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
